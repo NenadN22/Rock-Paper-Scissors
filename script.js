95d777314d155fee86c759a9a8ce4f869ccf93aa
NenadN22 <tonbe22@gmail.com>
2025-01-23 22:23:25 +0100

    Make play round function so after every iteration know who win that round player or computer

--- script.js
+++ script.js
@@ -9,10 +9,31 @@ function getComputerChoice(number) {
     }
 }
 
-console.log(getComputerChoice(3))
-function getHumanChoice(humanChoice) {
-    humanChoice = prompt('Rock,Paper or Scissors?')
-    return humanChoice
+function getHumanChoice() {
+    let = humanPromptChoice = prompt('Rock,Paper or Scissors?')
+    return humanPromptChoice[0].toUpperCase() + humanPromptChoice.slice(1);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+    
+    if (humanChoice == computerChoice) {
+        console.log(`It's a draw`)
+    }
+     else if (humanChoice === 'Rock' && computerChoice === 'Scissors') {
+        console.log(`You Win  ${humanChoice} beat ${computerChoice}` )
+    } else if  (humanChoice === 'Paper' && computerChoice === 'Rock') {
+        console.log(`You Win  ${humanChoice} beat ${computerChoice}` )
+    } else if (humanChoice === 'Scissors' && computerChoice === 'Paper') {
+        console.log(`You Win  ${humanChoice} beat ${computerChoice}` )
+    } else {
+        console.log(`You Lose  ${computerChoice} beat ${humanChoice} ` )
+    }
 
 }
-console.log(getHumanChoice())
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection,computerSelection)
