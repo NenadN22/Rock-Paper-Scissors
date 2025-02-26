e1175ae2f4362a4e5fd0a557a86571639b07206f
NenadN22 <tonbe22@gmail.com>
2025-02-26 23:39:28 +0100

    Make round result append on div inside html so display on site not only in console

--- script.js
+++ script.js
@@ -8,68 +8,67 @@ function getComputerChoice(number) {
          return 'Scissors'
     }
 }
-
+const display = document.querySelector('.display')
 const buttons = document.querySelectorAll('button')
-let humanButtonChoice = ''
+let playerButtonChoice = '';
 buttons.forEach(button => {
-    button.addEventListener('click', () => {; 
-    humanButtonChoice = button.textContent;
-    console.log(humanButtonChoice)
-    playRound(humanButtonChoice,getComputerChoice(3))
-  
-})
+    button.addEventListener('click', () => {
+        playerButtonChoice = button.textContent;
+       if (computerScore < 5 &&  humanScore < 5) {
+        playRound(playerButtonChoice,getComputerChoice(3))
+        } else {
+            return;
+        }
+    })
     
-
 })
+let roundResult = document.createElement('h3')
+
 
 
 function playRound(humanChoice,computerChoice) {
     
+    
     if (humanChoice == computerChoice) {
-        console.log(`It's a draw`)
+        roundResult.textContent =  (`It's a draw`)
     }
+    
      else if (humanChoice === 'Rock' && computerChoice === 'Scissors') {
-        console.log(`You Win  ${humanChoice} beat ${computerChoice}` )
+        roundResult.textContent = (`You Win  ${humanChoice} beat ${computerChoice}` )
         humanScore ++
     } else if  (humanChoice === 'Paper' && computerChoice === 'Rock') {
-        console.log(`You Win  ${humanChoice} beat ${computerChoice}` )
+        roundResult.textContent = (`You Win  ${humanChoice} beat ${computerChoice}` )
         humanScore ++
     } else if (humanChoice === 'Scissors' && computerChoice === 'Paper') {
-        console.log(`You Win  ${humanChoice} beat ${computerChoice}` )
+        roundResult.textContent = (`You Win  ${humanChoice} beat ${computerChoice}` )
         humanScore ++
     } else {
         
         computerScore ++
-        console.log (`You Lose  ${computerChoice} beat ${humanChoice} ` )
+        roundResult.textContent = (`You Lose  ${computerChoice} beat ${humanChoice} ` )
     }
-    for (let i = 0;  i < 5; i++ ){
-        console.log(humanScore)
-        console.log(computerScore)
-
-    }
+  
     if (humanScore > computerScore) {
         winner =`Good job you win,your score is  ${humanScore} vs computer score ${computerScore}`;
     } else if (computerScore > humanScore) {
         winner =`Sorry you lose,computer score is  ${computerScore} vs your score ${humanScore}`;
         
     } else {
-        winner = `It's a draw computer score is  ${computerScore} vs your score ${humanScore}` 
+        winner = `It's a draw ${computerScore} vs your score ${humanScore}` 
     }
-    console.log(winner)
+    let gameWinner = '';
+    if (computerScore === 5 ) {
+        console.log(gameWinner = `Sorry you lost game winner is Computer by  ${computerScore} vs ${humanScore}`)
+        return;
+        
+    } if (humanScore === 5) {
+        console.log(gameWinner = `Nice job you are game winner by ${humanScore} vs  ${computerScore}  `)
+        return;
+
+    }
+    
 
 }
 let humanScore = 0;
 let computerScore = 0;
-
-// function playGame() {
-//     let winner;
-    
-    
-    
-  
-
-
-
-
-// }
-// playGame();
+display.appendChild(roundResult)
